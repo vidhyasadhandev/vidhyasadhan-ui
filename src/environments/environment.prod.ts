6e63dbbe0552b498ae195084fae03a7ac3ffa45a
Praveen Suram <praveenkumars219@gmail.com>
2020-08-02 15:33:31 -0400

    changed url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: `http://api.vidhyasadhan.com/api`,
+  apiUrl: `https://api-vidhyasadhan.azurewebsites.net/api`,
   applicationServerPublicKey: `BK1AabF3PRWyqtFwz8-8NvCdG35Ze5GbRW6YdN1cqSsws2H3nIYNGiYnmTD45XRcul556ZUk2p8oURhVlaC4tWI`
 };
 
 // `http://vidhyasadhan.azurewebsites.net`
+//http://api.vidhyasadhan.com/api
 
