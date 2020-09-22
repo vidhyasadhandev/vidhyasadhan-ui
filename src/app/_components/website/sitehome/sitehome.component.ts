03441f3511f1343a63e3ce846d9d6e420a294255
Praveen Suram <praveenkumars219@gmail.com>
2020-09-22 00:10:39 -0400

    website changes

--- src/app/_components/website/sitehome/sitehome.component.ts
+++ src/app/_components/website/sitehome/sitehome.component.ts
@@ -9,9 +9,23 @@ export class SitehomeComponent implements OnInit {
 
   constructor() { }
   date: Date;
+  images = [];
 
   ngOnInit(): void {
     this.date = new Date();
+    this.loadImages();
+  }
+
+  loadImages(){
+    for (let index = 0; index < 4; index++) {
+      const element = {
+        id: index,
+        image1: 'assets/website/bg-svg/mg-' + (index + 1) + '.png',
+        image2: 'assets/website/bg-svg/mg-' + (index + 1) + '-1.png 2x,' + 'assets/website/bg-svg/mg-' + (index + 1) + '-2.png 3x',
+        image3: 'assets/website/bg-svg/mg-' + (index + 1) + '-2',
+      };
+      this.images.push(element);
+    }
   }
 
 }
